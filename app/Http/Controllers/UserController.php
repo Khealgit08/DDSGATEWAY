a6d2f0f9d9329aa4f2cf880b8f9e0d1ce9e8e56a
Khealgit08 <castilmichael08@gmail.com>
2025-03-28 10:00:09 +0800

    code changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,24 +2,56 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\Request;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+use DB;
+
 
 class UserController extends Controller {
-    public function getUsers() {
-        return response()->json(User::all(), 200);
+
+    use ApiResponser;
+
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
     }
 
-    public function addUser(Request $request) {
-        $this->validate($request, [
+    public function getUsers()
+    {
+        $users = User::all();
+        return response()->json($users, 200);
+    }
+
+    /**
+    * Return the list of users
+    * @return Illuminate\Http\Response
+    */
+    public function index()
+    {
+        $users = User::all();
+        return $this->successResponse($users);
+    }
+
+    public function add(Request $request ){
+        $rules = [
             'username' => 'required|max:20',
             'password' => 'required|max:20',
-            'gender'   => 'required|in:Male,Female',
-        ]);
+            'gender' => 'required|in:Male,Female',
+        ];
+        $this->validate($request,$rules);
         $user = User::create($request->all());
-        return response()->json($user, 201);
+        return $this->successResponse($user,Response::HTTP_CREATED);
     }
 
+    /**
+    * Obtains and show one user
+    * @return Illuminate\Http\Response
+    */
     public function getUserById($id) {
         $user = User::find($id);
     
@@ -29,5 +61,60 @@ class UserController extends Controller {
     
         return response()->json($user, 200);
     }
-    
+
+    /**
+    * Update an existing author
+    * @return Illuminate\Http\Response
+    */
+    public function update(Request $request,$id){
+        $rules = [
+            'username' => 'max:20',
+            'password' => 'max:20',
+            'gender' => 'in:Male,Female',
+        ];
+        $this->validate($request, $rules);
+        $user = User::findOrFail($id);
+        $user->fill($request->all());
+        // if no changes happen
+        if ($user->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        $user->save();
+        return $this->successResponse($user);
+        // old code
+        $this->validate($request, $rules);
+        //$user = User::findOrFail($id);
+        $user = User::where('userid', $id)->first();
+        if($user){
+            $user->fill($request->all());
+            // if no changes happen
+            if ($user->isClean()) {
+                return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+            $user->save();
+            return $this->successResponse($user);
+        }
+        else{
+            return $this->errorResponse('User ID Does Not Exists', Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    /**
+    * Remove an existing user
+    * @return Illuminate\Http\Response
+    */
+    public function delete($id){
+        $user = User::findOrFail($id);
+        $user->delete();
+        return $this->successResponse($user);
+        // old code
+        $user = User::where('userid', $id)->first();
+        if($user){
+            $user->delete();
+            return $this->successResponse($user);
+        }
+        {
+            return $this->errorResponse('User ID Does Not Exists', Response::HTTP_NOT_FOUND);
+        }
+    }
 }
