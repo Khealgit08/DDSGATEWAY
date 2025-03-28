a3acb10ad10da373a6d5142e8849baf010cbf943
Khealgit08 <castilmichael08@gmail.com>
2025-03-28 14:00:06 +0800

    back to normal

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,39 +2,17 @@
 
 namespace App\Providers;
 
-<<<<<<< HEAD
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Route;
-=======
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Facades\Route;
->>>>>>> 076df5d16a09c43536c98cfb2bd0c5558469212f
 
-class RouteServiceProvider extends ServiceProvider
+class AppServiceProvider extends ServiceProvider
 {
-    public function boot()
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
     {
-        parent::boot();
-    }
-
-    public function map()
-    {
-        $this->mapApiRoutes();
-        $this->mapWebRoutes();
-    }
-
-    protected function mapWebRoutes()
-    {
-        Route::middleware('web')
-            ->namespace($this->namespace)
-            ->group(base_path('routes/web.php'));
-    }
-
-    protected function mapApiRoutes()
-    {
-        Route::prefix('api')
-            ->middleware('api')
-            ->namespace($this->namespace)
-            ->group(base_path('routes/api.php'));
+        //
     }
 }
