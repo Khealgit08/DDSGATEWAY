15168e39edb3f53fdaac3214b4996be5a18b2292
Khealgit08 <castilmichael08@gmail.com>
2025-03-29 02:18:06 +0800

    New changes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,15 +4,8 @@ namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
 
-class Controller extends BaseController
-{
-<<<<<<< HEAD
-    //
-}
-=======
+class Controller extends BaseController {
     protected function successResponse($data, $code = 200){
         return response()->json($data, $code);
     }
-
 }
->>>>>>> 9205812dce7388f65852bf25d2dde0c04963bc99
