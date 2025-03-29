95e6b1004abeed81a26328670db77dcfa74b5d29
Khealgit08 <castilmichael08@gmail.com>
2025-03-29 22:14:11 +0800

    aadd changes

--- app/Services/User1Service.php
+++ app/Services/User1Service.php
@@ -15,10 +15,34 @@ class User1Service {
     }
 
     /**
+    * Create one user using the User1 service
+    * @return string
+    */
+    public function createUser1($data){
+    return $this->performRequest('POST', '/users1', $data);
+    }
+
+    /**
     * Obtain the full list of Users from User1 Site
     * @return string
     */
     public function obtainUsers1() {
         return $this->performRequest('GET','/users1'); //<—this code will call the GET localhost:8001/users (our site1)
+    }
+
+    /**
+    * Update an instance of user1 using the User1 service
+    * @return string
+    */
+    public function editUser1($data, $id) {
+        return $this->performRequest('PUT', "/users1/{$id}", $data);
+    }
+
+    /**
+    * Remove an existing user
+    * @return Illuminate\Http\Response
+    */
+    public function delete($id) {
+        return $this->performRequest('DELETE', "/users1/{$id}");
     }
 }
--- app/Services/User2Service.php
+++ app/Services/User2Service.php
@@ -14,6 +14,14 @@ class User2Service {
     public function __construct() {
         $this->baseUri =config('USERS2_SERVICE_BASE_URL');
     }
+    
+    /**
+    * Create one user using the User1 service
+    * @return string
+    */
+    public function createUser2($data){
+        return $this->performRequest('POST', '/users2', $data);
+    }
 
     /**
     * Obtain the full list of Users from User1 Site
@@ -21,5 +29,21 @@ class User2Service {
     */
     public function obtainUsers2() {
         return $this->performRequest('GET','/users2'); //<—this code will call the GET localhost:8001/users (our site1)
+    }
+
+    /**
+    * Update an instance of user1 using the User1 service
+    * @return string
+    */
+    public function editUser2($data, $id) {
+        return $this->performRequest('PUT', "/users2/{$id}", $data);
+    }
+
+        /**
+    * Remove an existing user
+    * @return Illuminate\Http\Response
+    */
+    public function delete($id) {
+        return $this->performRequest('DELETE', "/users1/{$id}");
     }
 }
